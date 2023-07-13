5ddaf10fc66b21e0703f112426cdec310f5809cd
FelipeBorz12 <boterofelipe88@gmail.com>
2023-07-13 16:01:02 -0500

    square,board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import './App.css'
 
+const Turns={
+  X:"x",
+  O:"o"
+}
+
+const board=array(9).fill(null)
+
+const Square=({children,updateBoard, index})=>{
+  return (
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+
 function App() {
-  return <h1>Tres en raya</h1>
+  return(
+    <main classname='board'>
+      <h1>Tres en raya</h1>
+      <section className='game'>
+        {
+          board.map((_, index)=>{
+            return(
+              <Square
+                key={index}
+                index={index}
+              >
+                {index}
+              </Square>  
+            )
+          })
+        }
+      </section>  
+    </main>
+  ) 
 }
 
 export default App
